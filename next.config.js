f038dc286c80a44b3e07e003946de227d138975d
Sergei928 <sergeiromanoff.job@gmail.com>
2024-12-19 18:36:25 +0000

    updated project

---
+++ next.config.js
@@ -0,0 +1,36 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    unoptimized: false,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "uploadthing.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "img.clerk.com",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "subdomain",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "files.stripe.com",
+        pathname: "/**",
+      },
+    ],
+  },
+  reactStrictMode: false,
+};
+
+module.exports = nextConfig;
---
+++ src/app/page.tsx
@@ -0,0 +1,8 @@
+"use client";
+export default function Page() {
+  return (
+    <main>
+      <h1>Plura</h1>
+    </main>
+  );
+}
